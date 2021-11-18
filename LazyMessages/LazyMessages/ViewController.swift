ba8e8b69eaf648b5473c62e4d584b6fc720d6dc6
BradR07 <94597662+BradR07@users.noreply.github.com>
2021-11-18 09:17:00 +0100

    Add new proj

---
+++ LazyMessages/LazyMessages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyMessages
+//
+//  Created by Brandon Reynier on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
