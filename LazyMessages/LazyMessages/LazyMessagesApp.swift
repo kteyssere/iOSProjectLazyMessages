ea8b68eed69ef38816287aeb16aa94afdebc1452
BradR07 <94597662+BradR07@users.noreply.github.com>
2021-11-18 09:01:47 +0100

    Ajout projet

---
+++ LazyMessages/LazyMessages/LazyMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyMessagesApp.swift
+//  LazyMessages
+//
+//  Created by Brandon Reynier on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LazyMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
